cd6aa6d81c8adc02b9c6122b6ab2bcdc9be5ed90
jaykhedekar7 <jaykhedekar@hotmail.com>
2020-06-30 17:16:32 +0530

    Unnecessary changes

--- database.py
+++ database.py
@@ -1,3 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
 
 class Registrations(db.Model):
     sno = db.Column(db.Integer, primary_key = True)
